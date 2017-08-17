17fcf3e05791f3425a7916502750a14427b7482f
joycebrum <joycebrumu.u@gmail.com>
2017-08-16 21:38:09 -0300

    Plataformas e player acertados
    coloquei as plataformas como knematic e o player como dynamic, assim
    eles colidem mas nao interferem um no outro fisicamente falando
    alem disso, consertei a rotação do mariozinho

--- Disquete, O Jogo/Assets/Scripts/PlataformaMovimento.cs
+++ Disquete, O Jogo/Assets/Scripts/PlataformaMovimento.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlataformaMovimento : MonoBehaviour {
 
     public float velocidade;
-    public int tipo; //1 = vertical, 2 = horizontal
+    public string tipo; //mudei para string
 
     private Vector2 moviment;
     private Rigidbody2D rigidbody;
@@ -21,21 +21,21 @@ public class PlataformaMovimento : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()//plataforma de movimento vertical
     {
-        if(tipo == 2 && (transform.position.x >= 60 || transform.position.x <= 47))
+        if(tipo == "Horizontal" && (transform.position.x >= 60 || transform.position.x <= 47))
         {
             velocidade = velocidade * (-1);
         }
-        if(tipo == 1 && transform.position.y>=40)
+        if(tipo == "Vertical" && transform.position.y>=40)
         {
             transform.position = new Vector3(transform.position.x,0,0);
         }
       
         switch (tipo)
         {
-            case 1:
+            case "Vertical":
                 moviment = new Vector2(0f, velocidade);
                 break;
-            case 2:
+            case "Horizontal":
                 moviment = new Vector2(velocidade, 0f);
                 break;
         }
